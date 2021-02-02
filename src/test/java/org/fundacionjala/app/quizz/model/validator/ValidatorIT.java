824a0b08d3c89f40f5bf3e8c0cf1e39c981e5f9a
Adriana Iris Escobar <lirio1818@gmail.com>
2021-02-02 19:27:15 -0400

    test added

--- src/test/java/org/fundacionjala/app/quizz/model/validator/ValidatorIT.java
+++ src/test/java/org/fundacionjala/app/quizz/model/validator/ValidatorIT.java
@@ -3,10 +3,15 @@ package org.fundacionjala.app.quizz.model.validator;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.fundacionjala.app.quizz.model.Question;
+import org.fundacionjala.app.quizz.model.QuestionType;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class ValidatorIT {
+    private List<ValidatorType> validations;
 
     @Test
     public void testDateValidator() {
@@ -17,5 +22,16 @@ public class ValidatorIT {
 
         Assert.assertEquals(DateValidator.class, dateValidator.getClass());
         Assert.assertTrue(errors.isEmpty());
+    }
+    @Test
+    public void getTitle() {
+        Question question = new Question("iris", QuestionType.TEXT, validations);
+        assertEquals("iris", question.getTitle());
+    }
+
+    @Test
+    public void getType() {
+        Question question = new Question("iris", QuestionType.DATE, validations);
+        assertEquals(QuestionType.DATE, question.getType());
     }
 }
